7b5c3c5b5e3bf9c0f8315d38a2fa6d9e1837753b
Aleksey-M <malekseyg@gmail.com>
2020-09-09 14:09:43 +0300

    V 2.1

---
+++ wwwroot/SaveFile.js
@@ -0,0 +1,22 @@
+﻿function fileSaveAs(filename, fileContent) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(fileContent)
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function uploadFileJS(element) {
+
+    let file = element.files[0];
+    let reader = new FileReader();
+
+    reader.addEventListener("loadend", function () {
+        let tmpField = document.getElementById('tempField');
+        tmpField.value = reader.result;
+        tmpField.dispatchEvent(new Event('change'));
+    });
+
+    reader.readAsText(file);
+}
